31a214f56b0f628d44053c22bd21f177dd9f806b
Daniel Castaneda <danielcr100@gmail.com>
2019-09-30 20:03:06 -0600

    primer commit curso cinta blanca DEVf.f

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// let var1= "Nombre";
+// console.log(var1)
+// let var2= "Email";
+// console.log(var2)
+// let var3= "Telefono";
+// console.log(var3)
+
+// let Nombre = prompt("Nombre de Contacto");
+// console.log(Nombre)
+// let email = prompt("e-mail de Contacto");
+// console.log(email)
+// let telefono = prompt("telefono de Contacto");
+// console.log(telefono)
+
+
+// Funcion
+
+let Nombre = prompt("Nombre de Contacto");
+
+let Apellido = prompt("apellido de contacto")
+
+function nombrecompleto(Nombre, Apellido){
+    return "Su nombre completo es " + Nombre + " "+ Apellido 
+}
+console.log(nombrecompleto(Nombre , Apellido ))
